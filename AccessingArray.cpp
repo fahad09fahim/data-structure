3369998212a483117c0b994691b8b072890cb7a9
fahad09fahim <anik.fahim07@gmail.com>
2024-11-30 01:21:10 +0600

    accessing oneD array

---
+++ AccessingArray.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[50], n, i, j;
+    cout << "how many data you want to insert: ";
+    cin >> n;
+    for (i = 1; i <= n; i++)
+    {
+        cin >> arr[i];
+    }
+    cout << "Array Data elements are:" << endl;
+    for (j = 1; j <= n; j++)
+    {
+        cout << "Elements: " << arr[j] << endl;
+    }
+}
